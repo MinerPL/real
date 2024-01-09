742f0d20de49608d0a75c84b94a15d70d9d4592a
DiscrapperManager <admin@minerpl.xyz>
2024-01-09 09:23:12 +0100

    canaryRelease: 258125

--- chunks/474775.js
+++ chunks/474775.js
@@ -15,7 +15,7 @@
             function d() {
                 var e, t, s, n, d, u;
                 let c = window.GLOBAL_ENV.RELEASE_CHANNEL,
-                    S = (e = "035c75704e01766e7524c1c7e5878d31694f9f23", e.substring(0, 7)),
+                    S = (e = "88c8fea896ccd2d7ab700954209e1dc6350e3a08", e.substring(0, 7)),
                     E = null === r.default || void 0 === r.default ? void 0 : r.default.remoteApp.getVersion(),
                     f = null === r.default || void 0 === r.default ? void 0 : null === (t = (s = r.default.remoteApp).getBuildNumber) || void 0 === t ? void 0 : t.call(s),
                     m = null === r.default || void 0 === r.default ? void 0 : null === (n = (d = r.default.remoteApp).getAppArch) || void 0 === n ? void 0 : n.call(d),
@@ -28,7 +28,7 @@
                         className: o.line,
                         variant: "text-xs/normal",
                         color: "text-muted",
-                        children: [c, " ", "258123", " ", (0, a.jsxs)("span", {
+                        children: [c, " ", "258125", " ", (0, a.jsxs)("span", {
                             className: o.versionHash,
                             children: ["(", S, ")"]
                         })]
