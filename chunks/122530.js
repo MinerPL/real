8e51e470421239386f844791a342f79022b9643f
DiscrapperManager <admin@minerpl.xyz>
2024-01-18 01:10:35 +0100

    canaryRelease: 259590

--- chunks/122530.js
+++ chunks/122530.js
@@ -9,20 +9,27 @@ n.r(t), n.d(t, {
   }
 });
 var s = n("599110"),
-  a = n("49111"),
-  o = n("353927");
+  a = n("311482"),
+  o = n("49111");
 
 function l(e, t, n) {
-  s.default.track(a.AnalyticEvents.VIDEO_TOGGLED, {
+  let {
+    featureEnabled: l,
+    windowLength: u,
+    allowedPoorFpsRatio: c,
+    fpsThreshold: d,
+    backoffTimeSec: f
+  } = a.default.getConfig(!1);
+  s.default.track(o.AnalyticEvents.VIDEO_TOGGLED, {
     video_toggle_reason: t,
     toggled_user_id: e,
     rtc_connection_id: null == i ? void 0 : i(),
     media_session_id: null == r ? void 0 : r(),
-    video_health_manager_window_length: null,
-    video_health_manager_poor_fps_ratio: null,
-    video_health_manager_fps_threshold: null,
+    video_health_manager_window_length: l ? u : null,
+    video_health_manager_poor_fps_ratio: l ? c : null,
+    video_health_manager_fps_threshold: l ? d : null,
     is_video_shown: n,
-    video_health_manager_backoff_time_seconds: null
+    video_health_manager_backoff_time_seconds: l ? f : null
   })
 }
 
